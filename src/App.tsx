d83c5b169c089b80f102bfa7f477fe47258db9b4
Ansh Goyal <anshgoyal1704@gmail.com>
2023-10-24 18:24:21 +0530

    add fireflies
    Signed-off-by: Ansh Goyal <anshgoyal1704@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 import { Header } from './components/Header/Header';
+import Fireflies from './components/Fireflies/Fireflies';
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
       <Header />
+      <Fireflies />
       <Router />
     </MantineProvider>
   );
---
+++ src/components/Fireflies/Fireflies.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Fireflies.css';
+
+function Fireflies() {
+  return (
+    <div>
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+      <div className="firefly" />
+    </div>
+  );
+}
+
+export default Fireflies;
